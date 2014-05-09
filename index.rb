2a0e7625846000db85cf61dd753428375a4877f0
Jacob Bijani <jacob.bijani@gmail.com>
2014-05-08 22:34:39 -0400

    site

--- index.rb
+++ index.rb
@@ -1,8 +1,10 @@
 <html>
 <head>
-    <title>mondays</title>
+    <meta charset="utf-8">
+    <title>Mondays</title>
+    <style type="text/css">body{display:table;width:100%;height:100%}span{display:table-cell;vertical-align:middle;text-align:center;font-size:100px;font-family:"helvetica neue";font-weight:500}</style>
 </head>
 <body>
-    mondays
+    <span>Mondays</span>
 </body>
 </html>
