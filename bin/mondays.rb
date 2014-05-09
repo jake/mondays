d5d89b6f3cee6ff622069337f5b8137ac1072e96
Jacob Bijani <jacob.bijani@gmail.com>
2014-05-08 22:50:01 -0400

    percent of year remaining

--- bin/mondays.rb
+++ bin/mondays.rb
@@ -9,6 +9,11 @@ def commas(number)
   number.to_s.chars.to_a.reverse.each_slice(3).map(&:join).join(",").reverse
 end
 
+def percent(number)
+  percent = "%g" % ("%.2f" % number)
+  "#{percent}%"
+end
+
 # system config
 twilio_from_number  = ENV["TWILIO_FROM_NUMBER"] || "+15005550006"
 twilio_account_sid  = ENV["TWILIO_ACCOUNT_SID"] || "ACf9ed0115dbeba2e65361be75f4c82ea9"
@@ -28,6 +33,14 @@ retire  = born.advance(years: retirement_age)
 if today.monday?
   mondays = today.upto(retire).count(&:monday?)
   message = "You have #{commas mondays} Mondays left until you retire."
+end
+
+if today.wednesday?
+  day_of_year = today.strftime("%j")
+  elapsed     = (day_of_year.to_f / 366.0) * 100
+  remaining   = 100 - elapsed
+
+  message = "There is #{percent remaining} of the current year remaining."
 end
 
 if message.nil?
