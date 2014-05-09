e8db24bd996462304fe46ea88c8d75cec592afc9
Jacob Bijani <jacob.bijani@gmail.com>
2014-05-08 23:32:20 -0400

    start on seasons

--- bin/mondays.rb
+++ bin/mondays.rb
@@ -32,7 +32,7 @@ retire  = born.advance(years: retirement_age)
 
 if today.monday?
   mondays = today.upto(retire).count(&:monday?)
-  message = "You have #{commas mondays} Mondays left until you retire."
+  message = "There are #{commas mondays} Mondays left until you retire."
 end
 
 if today.wednesday?
@@ -41,6 +41,40 @@ if today.wednesday?
   remaining   = 100 - elapsed
 
   message = "There is #{percent remaining} of the current year remaining."
+end
+
+if false && today.friday?
+  # Spring - Mar, Apr, May
+  # Summer - Jun, Jul, Aug
+  # Autumn - Sep, Oct, Nov
+  # Winter - Dec, Jan and Feb
+
+  seasons = {
+    3 => :spring,
+    4 => :spring,
+    5 => :spring,
+
+    6 => :summer,
+    7 => :summer,
+    8 => :summer,
+
+    9  => :autumn,
+    10 => :autumn,
+    11 => :autumn,
+
+    12 => :winter,
+    1  => :winter,
+    2  => :winter,
+  }
+
+  current_season = seasons[today.month]
+  current_range  = seasons.select{ |month, season| season == current_season}
+
+  next_season = seasons[today.next_month.month]
+  next_range  = seasons.select{ |month, season| season == next_season}
+
+  puts current_season, current_range
+  puts next_season, next_range
 end
 
 if message.nil?
